dc5a050860a683208b85ac6b681cacffd23160bd
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2024-12-23 21:09:54 +0600

    commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.get('/test', (req, res) => {
+    res.send('Hello World');
+ })
+
+app.post('/test', (req, res) => {
+    res.send('Got a POST request');
+ })
+
+app.put('/test', (req, res) => {
+        res.send('Got a PUT request');
+    })
+
+app.delete('/test', (req, res) => {
+        res.send('Got a DELETE request');
+    })
+
 app.use((req,res)=>{
     res.send(`Hello World I'm a server`);
 })
